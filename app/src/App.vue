4106532f74b4efe45a789383d075f49515814d30
James <james.mahy@cevo.co.uk>
2020-06-15 19:41:31 +0100

    Initial Homepage

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,23 +1,77 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopNav from './components/TopNav.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopNav
   }
 }
 </script>
 
+<style>
+  @font-face {
+    font-family: 'koch_geometricregular';
+    src: url('./assets/fonts/kochgeometric_book_macroman/KochGeometric-Book-webfont.eot');
+    src: url('./assets/fonts/kochgeometric_book_macroman/KochGeometric-Book-webfont.eot?#iefix') format('embedded-opentype'),
+    url('./assets/fonts/kochgeometric_book_macroman/KochGeometric-Book-webfont.woff2') format('woff2'),
+    url('./assets/fonts/kochgeometric_book_macroman/KochGeometric-Book-webfont.woff') format('woff'),
+    url('./assets/fonts/kochgeometric_book_macroman/KochGeometric-Book-webfont.ttf') format('truetype'),
+    url('./assets/fonts/kochgeometric_book_macroman/KochGeometric-Book-webfont.svg#koch_geometricregular') format('svg');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'koch_geometricmedium';
+    src: url('./assets/fonts/kochgeometric_medium_macroman/KochGeometric-Medium-webfont.eot');
+    src: url('./assets/fonts/kochgeometric_medium_macroman/KochGeometric-Medium-webfont.eot?#iefix') format('embedded-opentype'),
+    url('./assets/fonts/kochgeometric_medium_macroman/KochGeometric-Medium-webfont.woff2') format('woff2'),
+    url('./assets/fonts/kochgeometric_medium_macroman/KochGeometric-Medium-webfont.woff') format('woff'),
+    url('./assets/fonts/kochgeometric_medium_macroman/KochGeometric-Medium-webfont.ttf') format('truetype'),
+    url('./assets/fonts/kochgeometric_medium_macroman/KochGeometric-Medium-webfont.svg#koch_geometricmedium') format('svg');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  body{
+    color: #fff !important;
+    font-family: "koch_geometricregular","Myriad Pro",Arial !important;
+    background-color: #121211 !important;
+  }
+
+  #top_container{
+    background-image: url(./assets/facesfull.jpg);
+    background-size: 50%;
+      min-height: 500px;
+  }
+
+  #top_container a{
+    color:#fff;
+  }
+
+  #top_container h1, #top_container h2, #top_container h3, #top_container h4, #top_container h5 {
+    color: #fff;
+  }
+
+  #top_container h2{
+      font-size:1.25rem;
+  }
+</style>
+
 <template>
   <div id="app">
-    <div class="container">
-      <div class="row">
-        <div class="col-12 mx-auto text-center">
-          <img alt="Vue logo" src="./assets/logo.png">
-          <HelloWorld msg="Welcome to Your Vue.js App"/>
-        </div>
-      </div>
-    </div>
+    <b-container class="pb-4" id="top_container" fluid>
+        <TopNav />
+        <b-row>
+          <div class="mx-auto py-4">
+              <b-col lg="8" class="mx-auto text-center">
+                <h1 class="mb-4">SoSa is a fun, friendly and privacy first community!</h1>
+                <h2 class="py-2">Making friends online should be fun, free and privacy conscious so we're developing a new kind of social network.</h2>
+                <h2 class="py-2">Follow SoSa's journey through the blog or our discord!</h2>
+              </b-col>
+          </div>
+        </b-row>
+    </b-container>
   </div>
 </template>
---
+++ app/src/components/TopNav.vue
@@ -0,0 +1,71 @@
+<script>
+    export default {
+        name: 'TopNav',
+        props: {}
+    }
+</script>
+
+<style>
+   #login_button {
+       border-radius: 1.2rem;
+       cursor: pointer;
+       padding: .5rem 1rem;
+       font-size: 1.1rem;
+       line-height: 1;
+       color: #fff;
+    }
+    .navbar-brand h1{
+        font-family: "koch_geometricmedium", "Myriad Pro", Arial;
+    }
+
+    .navbar-nav{margin: .25em 0;}
+    #navbar_left .nav-item {margin-left: 0.5em;}
+
+    .nav-item .nav-link {padding: 0 1em;}
+
+    #navbar_right .nav-item {
+        margin-right: 0.5em;
+    }
+
+    #navbar_right .nav-item .nav-link i{
+        font-size:1.75em;
+        color: #ccc;
+    }
+
+</style>
+<template>
+    <b-navbar toggleable="md" type="dark">
+        <b-navbar-brand class="text-center text-md-left">
+            <h1>SoSa</h1>
+        </b-navbar-brand>
+
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav class="ml-auto" id="navbar_right">
+                <b-nav-item href="https://blog.sosa.net">
+                    <i class="fal fa-newspaper"></i>
+                </b-nav-item>
+
+                <b-nav-item href="https://facebook.com/SoSaCommunity">
+                    <i class="fab fa-facebook-f"></i>
+                </b-nav-item>
+
+                <b-nav-item href="https://twitter.com/ChatPlayShare">
+                    <i class="fab fa-twitter"></i>
+                </b-nav-item>
+
+                <b-nav-item href="https://youtube.com/c/ChatPlayShare">
+                    <i class="fab fa-youtube"></i>
+                </b-nav-item>
+
+                <b-nav-item href="https://steamcommunity.com/groups/chatplayshare">
+                    <i class="fab fa-steam"></i>
+                </b-nav-item>
+
+                <b-nav-item href="https://www.linkedin.com/company/sosa-community/">
+                    <i class="fab fa-linkedin"></i>
+                </b-nav-item>
+            </b-navbar-nav>
+            <a class="btn btn-outline-warning" id="login_button">Login</a>
+        </b-collapse>
+    </b-navbar>
+</template>
