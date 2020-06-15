10a488782f936b886fb812ef132caabc876b80d4
James <james.mahy@cevo.co.uk>
2020-06-15 18:50:01 +0100

    Initial Vue Setup with Bootstrap

---
+++ app/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 mx-auto text-center">
+          <img alt="Vue logo" src="./assets/logo.png">
+          <HelloWorld msg="Welcome to Your Vue.js App"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
